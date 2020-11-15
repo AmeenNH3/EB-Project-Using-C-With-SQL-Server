22dfc0b874c7fd033cc8379ad9e9484a23ee0de7
Mohamed Ameenullah H <45750227+AmeenNH3@users.noreply.github.com>
2020-11-15 20:30:34 +0530

    Update DBHandler.cs

--- DBHandler.cs
+++ DBHandler.cs
@@ -13,7 +13,7 @@ namespace BillAutomation      //DO NOT change the namespace name
         //Implement the methods as per the description
         public SqlConnection getConnection(){
             string connectionString = @"Data Source=AMEEN;Initial Catalog=ElectrictyBillDB;Integrated Security=True";
-            Console.WriteLine("Db handler called");
+            //Console.WriteLine("Db handler called");
             return new SqlConnection(connectionString);
         }
     }
